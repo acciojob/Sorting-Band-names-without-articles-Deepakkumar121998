70138cc3d4090872403f79fbe36a8f221b0774fe
Deepakkumar121998 <130857964+Deepakkumar121998@users.noreply.github.com>
2023-07-19 00:08:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-() => {
-  cy.get("li").should("have.length.at.least", 1);
+let bandNames = ['The Beatles', 'Led Zeppelin', 'Aerosmith', 'The Rolling Stones', 'Pink Floyd'];
 
-  const tags_content = [];
-  const li_tags_count = document.getElementsByTagName('li').length;
+function sortBandNames(bandNames) {
+  let articlesPattern = /^(a|an|the)\s/i;
 
-  // Getting li tags content and checking if they are without any articles and in sorted order
-  for (let index = 0; index < li_tags_count; index++) {
-    cy.get("li").eq(index).then($el => {
-      tags_content.push($el.text());
-    });
-  }
-
-  tags_content.forEach(tag_content => {
-    const first_word = tag_content.split(" ")[0];
-    const not_article = first_word !== "A" && first_word !== "An" && first_word !== "The";
-    expect(not_article).to.be.equal(true);
+  bandNames.sort((a, b) => {
+    let nameA = a.replace(articlesPattern, '').trim();
+    let nameB = b.replace(articlesPattern, '').trim();
+    return nameA.localeCompare(nameB);
   });
+
+  return bandNames;
 }
+
+let ulElement = document.querySelector('#band');
+
+let sortedBandNames = sortBandNames(bandNames);
+sortedBandNames.forEach(bandName => {
+  let liElement = document.createElement('li');
+  liElement.textContent = bandName;
+  ulElement.appendChild(liElement);
+});
